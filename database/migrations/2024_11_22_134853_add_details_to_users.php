93080b6b507cf3f886f4fdf362516ffd629e4e2e
darshan45672 <drshnbhandary@gmail.com>
2024-11-22 21:59:32 +0530

    db

---
+++ database/migrations/2024_11_22_134853_add_details_to_users.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('usn')->nullable()->unique()->index();
+            $table->enum('semester', ['1', '2', '3', '4', '5', '6', '7', '8'])->nullable();
+            $table->string('phone')->unique()->nullable();
+            $table->string('image')->nullable();
+            $table->boolean('is_alumini')->default(false);
+            $table->boolean('is_admin')->default(false);
+            $table->boolean('is_verified')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('usn')->nullable()->unique()->index();
+            $table->enum('semester', ['1', '2', '3', '4', '5', '6', '7', '8'])->nullable();
+            $table->string('phone')->unique()->nullable();
+            $table->string('image')->nullable();
+            $table->boolean('is_alumini')->default(false);
+            $table->boolean('is_admin')->default(false);
+            $table->boolean('is_verified')->default(false);
+        });
+    }
+};
