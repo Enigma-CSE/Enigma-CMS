ea7d05ba78c9ea5e8f0921ab0fa211baabe0a795
darshan45672 <drshnbhandary@gmail.com>
2025-01-05 09:31:23 +0530

    updated user resource

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -6,11 +6,15 @@ use App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
 use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\ToggleButtons;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
@@ -23,19 +27,35 @@ class UserResource extends Resource
     protected static ?string $model = User::class;
 
     protected static ?string $navigationIcon = 'heroicon-s-user-plus';
-       
+
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-
-                TextInput::make('name')->required(),
-                TextInput::make('email')->email(),
-                TextInput::make('password')->password()->visibleOn('create'),
-                Select::make('name')->options([
-                    'test'=>'test as name',
-                    'youtube'=>'youtube as name',
-                ]),
+                Section::make('Student Informa')->schema([
+                    FileUpload::make('image')->image()->required()->directory('images/users')->openable()->preserveFilenames()
+                    ->downloadable()->columnSpanFull(),
+                    TextInput::make('name')->required(),
+                    TextInput::make('email')->email()->required(),
+                    TextInput::make('password')->password(),
+                    TextInput::make('usn')->required(),
+                    Select::make('semester')->options([
+                        '1' => '1',
+                        '2' => '2',
+                        '3' => '3',
+                        '4' => '4',
+                        '5' => '5',
+                        '6' => '6',
+                        '7' => '7',
+                        '8' => '8',
+                    ])->required()->default('1'),
+                    TextInput::make('phone')->required(),
+                ])->columns(2)->collapsible(),
+                Section::make('Student Role Settings')->schema([
+                    ToggleButtons::make('is_alumini')->boolean()->default(false)->inline(),
+                    ToggleButtons::make('is_admin')->boolean()->default(false)->inline(),
+                    ToggleButtons::make('is_verified')->boolean()->default(false)->inline(),
+                ])->columns(3)->collapsible(),
             ]);
     }
 
@@ -43,7 +63,7 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
-                TextColumn::make('id'),
+                ImageColumn::make('image')->circular(),
                 TextColumn::make('name'),
                 TextColumn::make('email'),
             ])
